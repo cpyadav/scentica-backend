55b0e52ea82ff777bfe6d2d20371311ca3c15a73
cp yadav <cpyadav2010@gmail.com>
2024-01-02 13:23:20 +0530

    fixed

--- dbConfig.js
+++ dbConfig.js
@@ -3,8 +3,8 @@ require('dotenv').config;
 const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'dev',
-    password: process.env.DB_PASSWORD || 'dev@123',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
     database: process.env.DB_NAME || 'scentia',
     connectionLimit: 10
 }
