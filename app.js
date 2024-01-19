d3c088e2eea56706992e96345e3a7896a3915632
cp yadav <cpyadav2010@gmail.com>
2024-01-19 09:22:19 +0530

    fixed

--- app.js
+++ app.js
@@ -115,9 +115,9 @@ app.post('/signup', async (req, res) => {
         // Execute the insert query
         const insertResults = await pool.query(insertQuery, insertValues);
         // Get the last inserted ID
-        const lastInsertedId = insertResults && (insertResults.insertId || (insertResults && insertResults[0] && insertResults[0].insertId));
+       // const lastInsertedId = insertResults && (insertResults.insertId || (insertResults && insertResults[0] && insertResults[0].insertId));
             // Generate JWT token
-            const token = jwt.sign({ id: lastInsertedId }, secretKey, { expiresIn: '1h' });
+            const token = jwt.sign({ id: 1 }, secretKey, { expiresIn: '1h' });
             res.setHeader('Authorization', `Bearer ${token}`);
             // Create response object
             const objToken = {
